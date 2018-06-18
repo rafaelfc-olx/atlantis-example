06c5ea57fd5287cef3309742c51d13074311d839
rafaelfc-olx <rafael.correa@olx.com>
2018-06-18 17:55:37 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+# Specify the provider and access details
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+  cidr_block = "10.0.0.0/16"
+}
